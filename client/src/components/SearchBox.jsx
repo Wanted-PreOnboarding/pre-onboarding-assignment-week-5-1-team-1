c50e7046fd98e287703c12998d90c3cef263207b
unsnruu <unsnruu@gmail.com>
2022-09-28 08:30:21 +0900

    [#2] feat : styled components 생성
    UI를 보여주기 위하여 styled components를 생성하였습니다.

--- client/src/components/SearchBox.jsx
+++ client/src/components/SearchBox.jsx
@@ -1,9 +1,55 @@
 import React from 'react';
 import styled from 'styled-components';
+import { BiSearch } from 'react-icons/bi';
 
 function SearchBox() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <StyledSearch />
+      <Input />
+      <Button>검색</Button>
+    </Container>
+  );
 }
 export default SearchBox;
 
-const Container = styled.div``;
+const Container = styled.div`
+  margin-top: 20rem;
+  background-color: white;
+  width: 28rem;
+  height: 4rem;
+  border-radius: 1rem;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`;
+
+const StyledSearch = styled(BiSearch)`
+  width: 4rem;
+  font-size: 2rem;
+  position: absolute;
+  left: 0;
+`;
+const Input = styled.input`
+  width: 100%;
+  margin: 0 4.5rem 0 3rem;
+  padding: 1rem;
+  border: none;
+  font-size: 1.5rem;
+  &:focus {
+    outline: none;
+  }
+`;
+const Button = styled.button`
+  width: 5em;
+  height: 100%;
+  position: absolute;
+  right: 0;
+  border: none;
+  background-color: #357ae1;
+  font-size: 1rem;
+  font-weight: bold;
+  color: white;
+`;
