5a41411b76c33a577280f3aff43000782a009f7c
unsnruu <unsnruu@gmail.com>
2022-09-28 09:28:11 +0900

    [#2] feat : RecommendItem 컴포넌트 생성

---
+++ client/src/components/RecommendItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function RecommendItem() {
+  return <Container></Container>;
+}
+export default RecommendItem;
+
+const Container = styled.div``;
